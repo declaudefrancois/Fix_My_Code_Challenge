ab1087f9347c385c8d8744f107ec342d2eeb4ae1
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-04-17 23:47:24 +0000

    (0x01): fixed getter and setter order in user.js and codestyle in square.js

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
+"""Square module.
+"""
 
-class square():
-    
+
+class Square():
+    """
+        Defines a squares.
+
+        Attributes:
+            width (int): the width
+            height (int): the height
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Instanciates a new square. """
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if key in ["width", "height"]:
+                setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def permiterOfMySquare(self):
+        """ Calculates and returns the square's perimeter. """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
+        """ Returns a string representationof the sqare. """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiterOfMySquare())
