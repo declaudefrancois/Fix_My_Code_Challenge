f85c7fd6b48ba271c33c5a8b5ca5b24c1afadfe4
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-07-18 22:35:46 +0100

    (0x01): Fix user & square classes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+""" Square module """
 
-class square():
-    
+
+class Square():
+    """ Square class  """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Init a new Square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Str representation of the square """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
